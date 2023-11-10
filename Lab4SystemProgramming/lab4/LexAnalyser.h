2b8a0e8b74ad87c09e0f6294548baa05215a5fbe
Mykhailo Lapshyn <lapshynmisha@gmail.com>
2023-11-10 23:01:23 +0100

    change definition of vector

--- Lab4SystemProgramming/lab4/LexAnalyser.h
+++ Lab4SystemProgramming/lab4/LexAnalyser.h
@@ -2,18 +2,19 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <utility>
 using namespace std;
 
 class LexAnalyser {
 private:
 	vector<string> terminals = {}; /*When reading the grammar, the two alphabets are read, the all the inference rules*/
 	vector<string> nonterminals = {};
-	vector<string, vector<string>> rules = {}; /*In my concept the rules are stored as pairs of nonterminal 
+	vector<pair<string, vector<string>>> rules = {}; /*In my concept the rules are stored as pairs of nonterminal
 	(left part of the rule) and vector of lexems (right part)*/
 
 	vector<string>epsilonNonTerm = {};
-	vector<string, vector<string>> firstKVec = {};
-	vector<string, vector<string>> followKVec = {}; /*these arrays are stored in the same way as the rules*/
+	vector<pair<string, vector<string>>> firstKVec = {};
+	vector<pair<string, vector<string>>> followKVec = {}; /*these arrays are stored in the same way as the rules*/
 	
 	//there should be some attributes for the syntax table
 	
--- Lab4SystemProgramming/lab4/lab4.cpp
+++ Lab4SystemProgramming/lab4/lab4.cpp
@@ -1,9 +1,11 @@
-﻿#include <iostream>
+#include <iostream>
+#include "LexAnalyser.h"
 using namespace std;
 
 int main()
 {
     //тут буде взаємодія з користувачем
+    
     return 0;
 }
 
