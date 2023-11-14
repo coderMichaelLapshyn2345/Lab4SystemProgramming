9e1444b44b163ceccab376fbcd649309acd91212
Danyil08 <73671039+Danyil08@users.noreply.github.com>
2023-11-14 18:43:37 +0200

    Update lab4.cpp
    Just code styling

--- Lab4SystemProgramming/lab4/lab4.cpp
+++ Lab4SystemProgramming/lab4/lab4.cpp
@@ -5,8 +5,8 @@ using namespace std;
 
 int main()
 {
-    //тут буде взаємодія з користувачем
-    string path, input;
+	//тут буде взаємодія з користувачем
+	string path, input;
 	printf("Enter file name (+ .txt) to read grammar from: ");
 	scanf(" %s", path.c_str());LexAnalyser grammar(path);
 	printf("Enter input string to analyse by given grammar: ");
