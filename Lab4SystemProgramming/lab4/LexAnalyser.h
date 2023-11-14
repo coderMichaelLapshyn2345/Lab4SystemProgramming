1b730feca34e910d74b47133fdba3667e7d83e96
Danyil08 <73671039+Danyil08@users.noreply.github.com>
2023-11-14 15:51:18 +0200

    Update LexAnalyser.h
    "char" instead of "string" for attributes of class

--- Lab4SystemProgramming/lab4/LexAnalyser.h
+++ Lab4SystemProgramming/lab4/LexAnalyser.h
@@ -8,14 +8,14 @@ using namespace std;
 
 class LexAnalyser {
 private:
-	vector<string> terminals = {}; /*When reading the grammar, the two alphabets are read, the all the inference rules*/
-	vector<string> nonterminals = {};
-	vector<pair<string, vector<string>>> rules = {}; /*In my concept the rules are stored as pairs of nonterminal
+	vector<char> terminals = {}; /*When reading the grammar, the two alphabets are read, the all the inference rules*/
+	vector<char> nonterminals = {};
+	vector<pair<char, vector<char>>> rules = {}; /*In my concept the rules are stored as pairs of nonterminal
 	(left part of the rule) and vector of lexems (right part)*/
 
-	vector<string>epsilonNonTerm = {};
-	vector<pair<string, vector<string>>> firstKVec = {};
-	vector<pair<string, vector<string>>> followKVec = {}; /*these arrays are stored in the same way as the rules*/
+	vector<char>epsilonNonTerm = {};
+	vector<pair<char, vector<char>>> firstKVec = {};
+	vector<pair<char, vector<char>>> followKVec = {}; /*these arrays are stored in the same way as the rules*/
 	
 	//there should be some attributes for the syntax table
 	
