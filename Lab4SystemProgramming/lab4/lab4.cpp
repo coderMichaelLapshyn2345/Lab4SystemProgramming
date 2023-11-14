1c0a8449a302a8cccb23c121f09b0adb3e44436b
Danyil08 <73671039+Danyil08@users.noreply.github.com>
2023-11-14 18:34:25 +0200

    Just code styling

--- Lab4SystemProgramming/lab4/lab4.cpp
+++ Lab4SystemProgramming/lab4/lab4.cpp
@@ -7,11 +7,11 @@ int main()
 {
     //тут буде взаємодія з користувачем
     string path, input;
-	printf("Enter file name (+ .txt) to read grammar from: ");
-	scanf(" %s", path.c_str());
-	LexAnalyser grammar(path);
-	printf("Enter input string to analyse by given grammar: ");
-	scanf(" %s", input.c_str());
+    printf("Enter file name (+ .txt) to read grammar from: ");
+    scanf(" %s", path.c_str());
+    LexAnalyser grammar(path);
+    printf("Enter input string to analyse by given grammar: ");
+    scanf(" %s", input.c_str());
 	grammar.Analysis(input);
 	system("pause");
     return 0;
